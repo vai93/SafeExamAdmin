37e3b64a9a5b0e6af8a1efa8e9638ad681a6a93f
vai93 <87764295+vai93@users.noreply.github.com>
2025-03-01 13:56:39 +0530

    Update firebase-admin-setup.js

--- firebase-admin-setup.js
+++ firebase-admin-setup.js
@@ -1,9 +1,11 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL
+  }),
   databaseURL: "https://141862631065.firebaseio.com"
 });
 

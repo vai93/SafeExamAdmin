d1b7c663261e122fba1b6eb3fb2270a61f0825d3
vai93 <87764295+vai93@users.noreply.github.com>
2025-03-01 18:16:30 +0530

    Update adminRegistration.js

--- js/adminRegistration.js
+++ js/adminRegistration.js
@@ -5,15 +5,18 @@ document.getElementById("registerForm").addEventListener("submit", async (event)
     const nuvID = document.getElementById("nuvID").value.trim();
     const password = document.getElementById("password").value;
     const confirmPassword = document.getElementById("confirmPassword").value;
-
+     loader.style.display = "flex";
+    spinner.style.display = "block";
     try {
         const response = await fetch("api/adminRegistration", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ name, nuvID, password, confirmPassword })
         });
-
+        
         const result = await response.json();
+        loader.style.display = "none";
+        spinner.style.display = "none";
         alert(result.message);
 
         if (response.ok) {
